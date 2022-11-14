37ec1e49de1b76b07a8ceff128196a9ef5766343
Junaid <72765785+junaid-hub@users.noreply.github.com>
2022-11-14 19:21:54 +0500

    added comments for better understanding

--- sendEmailUsingOutlook.py
+++ sendEmailUsingOutlook.py
@@ -1,10 +1,15 @@
 import smtplib
 
-def mail(sUsername,sPass,rUsername,subject,body):    
-        
+def sendMail(sUsername,sPass,rUsername,subject,body):    
+    # sUsername corresponds to email of sender
+    # spass corresponds to password of sender's email
+    # rUsername corresponds to email of reciever
+    # subject -> subject of mail
+    # body -> body o mail
+
+
     smtp_server = "smtp-mail.outlook.com"
     
-
     smtp = smtplib.SMTP(smtp_server, 587)
     smtp.starttls()
     smtp.login(sUsername, sPass)
@@ -15,6 +20,5 @@ def mail(sUsername,sPass,rUsername,subject,body):
     {body}"""
     smtp.sendmail(sUsername, rUsername, message)
             
-
     smtp.quit()
     
